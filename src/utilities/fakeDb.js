035e1f74a96886c6cf6f46cf98358c016fbc7221
Hamid Rahman <hamidz10101@gmail.com>
2022-10-22 21:13:25 +0600

    after local-storage

--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,17 +1,29 @@
 // local storage management system
 
 const fakeDb = id=>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-        console.log("Already exist")
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+    let shoppingCart;
+
+    const storedCart = localStorage.getItem('shopping-cart')
+    if (storedCart){
+        shoppingCart = JSON.parse(storedCart);
     }
     else{
-        localStorage.setItem(id, 1)
-        console.log('net item')
+        shoppingCart = {};
+    }
+    
+
+
+
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
     }
 
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export {fakeDb};
